c5a640843919cf60be1ba8a1901b7dc6de0d1905
Valentin <valentin.ctx@gmail.com>
2023-05-25 19:12:41 +0200

    home

--- app/src/main/java/com/example/gsb_menu/CompteRenduFragment.java
+++ app/src/main/java/com/example/gsb_menu/CompteRenduFragment.java
@@ -30,7 +30,7 @@ import java.util.Map;
 public class CompteRenduFragment extends Fragment {
 
     private static final String TAG = "CompteRenduFragment";
-    private static final String URL = "https://hugo-rivaux.fr/API/afficherCR.php";
+    private static final String URL = "http://gsb-sciencesu.alwaysdata.net/API/afficherCR.php";
     private RequestQueue mRequestQueue;
     private TableLayout mTableLayout;
 
